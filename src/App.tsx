bfd37022147b8035754774d9e4d3f5f13e928b88
Phuc <phuchoang2411@gmail.com>
2024-09-17 20:00:09 +0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,65 @@
-import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
-import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
-import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+import { ErrorComponent, Refine } from '@refinedev/core';
 
-import { useNotificationProvider } from "@refinedev/antd";
-import "@refinedev/antd/dist/reset.css";
+import {
+  ThemedLayoutV2,
+  useNotificationProvider,
+  ThemedTitleV2,
+} from '@refinedev/antd';
 
-import routerBindings, {
-  DocumentTitleHandler,
-  UnsavedChangesNotifier,
-} from "@refinedev/react-router-v6";
-import dataProvider from "@refinedev/simple-rest";
-import { App as AntdApp } from "antd";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { ColorModeContextProvider } from "./contexts/color-mode";
+import '@refinedev/antd/dist/reset.css';
+
+import routerBindings, { NavigateToResource } from '@refinedev/react-router-v6';
+import dataProvider from '@refinedev/simple-rest';
+import { App as AntdApp } from 'antd';
+import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
+
+import { PostCreate, PostEdit, PostList, PostShow } from './pages';
 
 function App() {
   return (
     <BrowserRouter>
-      <GitHubBanner />
-      <RefineKbarProvider>
-        <ColorModeContextProvider>
-          <AntdApp>
-            <DevtoolsProvider>
-              <Refine
-                notificationProvider={useNotificationProvider}
-                routerProvider={routerBindings}
-                dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
-                options={{
-                  syncWithLocation: true,
-                  warnWhenUnsavedChanges: true,
-                  useNewQueryKeys: true,
-                  projectId: "2316Rl-w1dE3j-RNWlvt",
-                }}
-              >
-                <Routes>
-                  <Route index element={<WelcomePage />} />
-                </Routes>
-                <RefineKbar />
-                <UnsavedChangesNotifier />
-                <DocumentTitleHandler />
-              </Refine>
-              <DevtoolsPanel />
-            </DevtoolsProvider>
-          </AntdApp>
-        </ColorModeContextProvider>
-      </RefineKbarProvider>
+      <AntdApp>
+        <Refine
+          notificationProvider={useNotificationProvider}
+          routerProvider={routerBindings}
+          dataProvider={dataProvider('https://api.fake-rest.refine.dev')}
+          resources={[
+            {
+              name: 'posts',
+              list: PostList,
+              show: PostShow,
+              create: PostCreate,
+              edit: PostEdit,
+              meta: {
+                canDelete: true,
+              },
+            },
+          ]}
+        >
+          <Routes>
+            <Route
+              element={
+                <ThemedLayoutV2
+                  Title={({ collapsed }) => (
+                    <ThemedTitleV2 collapsed={collapsed} text="Admin" />
+                  )}
+                >
+                  <Outlet />
+                </ThemedLayoutV2>
+              }
+            >
+              <Route index element={<NavigateToResource resource="posts" />} />
+              <Route path="posts">
+                <Route index element={<PostList />} />
+                <Route path="create" element={<PostCreate />} />
+                <Route path="edit/:id" element={<PostEdit />} />
+                <Route path="show/:id" element={<PostShow />} />
+              </Route>
+              <Route path="*" element={<ErrorComponent />} />
+            </Route>
+          </Routes>
+        </Refine>
+      </AntdApp>
     </BrowserRouter>
   );
 }
